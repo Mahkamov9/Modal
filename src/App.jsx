18490cd131e6ad24d2b0d911a6717aebba326089
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2022-11-20 16:51:14 +0500

    oz qoldi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react';
 import './App.css';
 import Modal from './components/modal/modal';
 
 function App() {
+
+const [open, setOpen] = useState(false)
+const [second, setSecond] = useState(false)
+
+
   return (
     <div className="App">
+
+      <div className='btnBox'>
+        <button className='appBtn' onClick={ () => setOpen(true) }>Qo'shish</button>
+        <button className='appBtn' onClick={ () => setSecond(true) }>Ayrish</button>
+      </div>
       
-      <Modal/>
+      <Modal modal={open} setModal={setOpen} topText='Subtitle' >
+        <form >
+          <input type="mailto" placeholder='mahkamov.dev@gmail.com' required />
+          <button>Submit</button>
+        </form>
+      </Modal>
+
+      <Modal modal={second} setModal={setSecond} topText='Title' >
+        <form >
+          <input type="text" placeholder='+998 93 191 97 37' required />
+          <button>Submit</button>
+        </form>
+      </Modal>
+
     </div>
   );
 }
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,16 +1,47 @@
+import { useEffect, useRef } from 'react';
 import './modal.css';
 
 
-function Modal () {
+function Modal ( {children, modal, setModal , topText} ) {
+
+    const modalWrapper = useRef(null);
+
+    const handleCloseModal = (evt) => {
+        if( evt.target===modalWrapper.current){
+            setModal(false)
+        }
+    }
+
+    useEffect(()=>{
+        const listener = (evt) =>{
+            console.log(evt.keyCode);
+            if(evt.keyCode === 27){
+                setModal(false)
+            }
+        }
+
+        if( modal){
+            window.addEventListener('keyup', listener)
+        }
+
+        return() => window.removeEventListener('keyup', listener);
+
+    }, [modal, setModal] );
+
+
     return(
-        <div className='modal-wrapper'>
+        <div 
+            ref={modalWrapper} 
+            className={`modal-wrapper ${modal && "modal-open"} `}
+            onClick={ handleCloseModal }
+            >
             <div className="modal">
                 <div className='modal__top'>
-                    <h3 className='modal__title'>Qo'shish</h3>
-                    <button className='modal__btn'> &times; </button>
+                    <h3 className='modal__title'> {topText} </h3>
+                    <button onClick={ ()=> {setModal(false)} } className='modal__btn'> &times; </button>
                 </div>
                 <div className='modal__content'>
-                    Modal content
+                    {children}
                 </div>
 
             </div>
